4aafd556bff63e5c68490728e6629370ca0d4d5c
ask-elad <aryanrai746@gmail.com>
2025-07-15 18:14:44 +0530

    some minor db changes

--- apps/https-backend/src/index.ts
+++ apps/https-backend/src/index.ts
@@ -3,34 +3,36 @@ import jwt from 'jsonwebtoken';
 import { middleware } from "./middleware";
 import {JWT_SECRET} from '@repo/backend-common/config';
 import { CreateUserSchema } from "@repo/common/types";
+import bcrypt from 'bcrypt';
+import { prismaClient } from '@repo/db/client'
  
 
 const app = express();
 app.use(express.json());
 
-app.post('/signup', (req,res)=>{
-    
-    const data = CreateUserSchema.safeParse(req.body);
-    if(!data.success){
-        message:"incorrect input"
+app.post("/signup", (req, res) => {
+
+    const parsedData = CreateUserSchema.safeParse(req.body);
+    if (!parsedData.success) {
+        res.json({
+        message: "Incorrect inputs"
+        });
+        return;
     }
 
-    // integrate db here
-})
-
-app.post('/signin', (req,res)=>{
-    const username = req.body.username;
-    const password = req.body.password;
-
-    const userId = 1;
-    const token = jwt.sign({
-        userId
-    },JWT_SECRET);
+    prismaClient.user.create({
+        data: {
+        email: parsedData.data?.email,
+        password: parsedData.data.password,
+        name: parsedData.data.name
+        }
+    });
 
     res.json({
-        token
-    })
-})
+        userId: "123"
+    });
+});
+
 
 app.post('/room',middleware,(req,res)=>{
     //db call
